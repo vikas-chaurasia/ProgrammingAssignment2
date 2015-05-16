4a56121bf309223f1b2da798f7a91f0846a525f1
vikas-chaurasia <vkschrs@gmail.com>
2015-05-16 15:24:11 +0530

    inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+
+
+## Caching the Inverse of a Matrix
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y)
+  {
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(inv) m<<-inv
+  getinverse<-function() m
+  list( set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+  
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+##makeCacheMatrix above. If the inverse has already been 
+##calculated (and the matrix has not changed), then cacheSolve 
+##should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
 }
+
+
+
+##source('E:/R/Data-Science/ProgrammingAssignment2/cachematrix.R')
+##mat<-makeCacheMatrix(matrix(c(1,2,3,4),c(2,2)))
+##cacheSolve(mat)
+
+##       [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+
